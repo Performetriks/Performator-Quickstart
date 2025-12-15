baaacbf50144504f1afaeeb69c80b0d1cec9e60f
Reto <reto.scheiwiller5@bluewin.ch>
2025-12-15 09:44:22 +0100

    put results all in a single folder

--- src/main/java/com/performetriks/performator/quickstart/globals/Globals.java
+++ src/main/java/com/performetriks/performator/quickstart/globals/Globals.java
@@ -21,7 +21,7 @@ public class Globals {
 	 
 	private static final String PACKAGE_DATA = "com.performetriks.performator.quickstart.data";
 	
-	public static final String DIR_RESULTS = "./target";
+	public static final String DIR_RESULTS = "./target/report";
 	public static final int REPORT_INTERVAL_SECONDS = 5;
 	
 	private static final Logger logger = (Logger) LoggerFactory.getLogger(Globals.class);
